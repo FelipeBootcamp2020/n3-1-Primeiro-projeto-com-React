fc1f8233280e41b6698970aa59bd033b2a781043
Felipe Genuino <felipe.genuino@softplan.com.br>
2020-06-06 06:31:05 -0300

    Estilizando dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,73 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImage from '../../assets/brand.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title> Dashboad </Title>;
+  return (
+    <>
+      <img src={logoImage} alt="vem tranquilo" />
+      <Title> Explore repositórios no Github </Title>
+
+      <Form action="">
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit"> Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/1197816?s=460&v=4"
+            alt="Felipe Genuino"
+          />
+          <div>
+            <strong>MyfidAcademy</strong>
+            <p>Repositório da academia myfid</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/1197816?s=460&v=4"
+            alt="Felipe Genuino"
+          />
+          <div>
+            <strong>MyfidAcademy</strong>
+            <p>Repositório da academia myfid</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/1197816?s=460&v=4"
+            alt="Felipe Genuino"
+          />
+          <div>
+            <strong>MyfidAcademy</strong>
+            <p>Repositório da academia myfid</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/1197816?s=460&v=4"
+            alt="Felipe Genuino"
+          />
+          <div>
+            <strong>MyfidAcademy</strong>
+            <p>Repositório da academia myfid</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
